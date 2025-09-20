1e359a29467214a76e0c693c4d86c49143d26802
SolitudeOracle <kaitaiyang52@gmail.com>
2025-09-20 14:40:18 +0800

    修改菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -12,11 +12,13 @@ import java.util.List;
 @Mapper
 public interface DishFlavorMapper {
 
-    @AutoFill(value = OperationType.INSERT)
     void insertBatch(List<DishFlavor> flavors);
 
     @Delete("delete from dish_flavor where dish_id = #{dishId}")
     void deleteByDishId(Long dishId);
 
     void deleteByDishIds(List<Long> dishIds);
+
+    @Select("select * from dish_flavor where dish_id = #{dishId}")
+    List<DishFlavor> getByDishId(Long dishId);
 }
